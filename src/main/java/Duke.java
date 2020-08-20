77c4fc89685d2a89ebb0b1cde7057e2d03553315
yokemin <woonyokemin@gmail.com>
2020-08-20 10:47:07 +0800

    Revert "Level 0. Greet"
    This reverts commit 7f4aa76f5ae752a88f10a5bf261ba6b06f409c86.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -6,16 +6,5 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
-
-        // Greet user
-        System.out.println("____________________________________________________________");
-        System.out.println("Hello! I'm Duke");
-        System.out.println("What can I do for you?");
-        System.out.println("____________________________________________________________");
-
-
-        // Say bye to user
-        System.out.println("Bye. Hope to see you again soon!");
-        System.out.println("____________________________________________________________");
     }
 }
