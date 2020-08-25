4b9c6a1235713066ba1069c645cca919aa80ab28
yokemin <woonyokemin@gmail.com>
2020-08-25 16:00:51 +0800

    Level 1. Greet, Echo, Exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,25 @@
+import java.util.Scanner;
+
 public class Duke {
+
+    public static String horizontalLine = "____________________________________________________________";
+
+    public static void echo(String userInput){
+        System.out.println(userInput);
+        System.out.println(horizontalLine);
+    }
+
+    public static int sayBye(String byeInput) {
+        if (byeInput.toLowerCase().contains("bye")) {
+            System.out.println("Bye. Hope to see you again soon!");
+            System.out.println(horizontalLine);
+            return 1;
+        }
+        else {
+            return 0;
+        }
+    }
+
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -8,14 +29,18 @@ public class Duke {
         System.out.println("Hello from\n" + logo);
 
         // Greet user
-        System.out.println("____________________________________________________________");
+        System.out.println(horizontalLine);
         System.out.println("Hello! I'm Duke");
         System.out.println("What can I do for you?");
-        System.out.println("____________________________________________________________");
+        System.out.println(horizontalLine);
 
+        // Level 1. Greet, Echo, Exit
+        Scanner scan = new Scanner(System.in);
+        String userInput = scan.nextLine();
+        do {
+            echo(userInput);
+            userInput = scan.nextLine();
+        } while (sayBye(userInput) == 0);
 
-        // Say bye to user
-        System.out.println("Bye. Hope to see you again soon!");
-        System.out.println("____________________________________________________________");
     }
 }
