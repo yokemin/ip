c3c24e8afb7c66dbb29220a7ecf6d23d860e81dc
yokemin <woonyokemin@gmail.com>
2020-09-24 14:36:16 +0800

    Add Ui class that deals with interactions with the user

---
+++ src/main/java/duke/command/Ui.java
@@ -0,0 +1,69 @@
+package duke.command;
+
+
+import java.util.Scanner;
+
+import static duke.Duke.HORIZONTAL_LINE;
+import static duke.command.Parser.KEYWORD_BYE;
+
+public class Ui {
+    private static final String logo = " ____        _        \n"
+                + "|  _ \\ _   _| | _____ \n"
+                + "| | | | | | | |/ / _ \\\n"
+                + "| |_| | |_| |   <  __/\n"
+                + "|____/ \\__,_|_|\\_\\___|\n";
+
+    public void greetUser(){
+        System.out.println("Hello from\n" + logo);
+        System.out.println(HORIZONTAL_LINE);
+        System.out.println("Hello! I'm Duke.");
+        System.out.println("What can I do for you?");
+        System.out.println(HORIZONTAL_LINE);
+    }
+
+    // ends program when reader says 'bye'
+    public boolean sayBye(String byeInput) {
+        if (byeInput.toLowerCase().contains(KEYWORD_BYE)) {
+            System.out.println("Bye. Hope to see you again soon!");
+            System.out.println(HORIZONTAL_LINE);
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public static String getUserInput() throws DukeException {
+
+        // Get first user input
+        Scanner scan = new Scanner(System.in);
+        String userInput = scan.nextLine();
+        Parser.handleErrorUserInputs(userInput);
+
+        // Subsequent user inputs
+        while (Parser.invalidInput) {
+            userInput = scan.nextLine();
+            try {
+                Parser.handleErrorUserInputs(userInput);
+            } catch (DukeException e) {
+                Parser.invalidInput = true;
+            }
+        }
+
+        return userInput;
+    }
+
+    public Ui() {
+    }
+
+    public void showLoadingError(String message) {
+        System.out.println(message);
+        System.out.println(HORIZONTAL_LINE);
+    }
+
+    public void showErrorMessage(String message) {
+        System.out.println(message);
+        System.out.println(HORIZONTAL_LINE);
+    }
+
+
+}
